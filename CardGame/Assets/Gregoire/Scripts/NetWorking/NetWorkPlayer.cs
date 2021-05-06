a8e2aa8fee10bbdd6ad622ddf8e27d48001b2054
Carla <carlita.b34@gmail.com>
2021-05-06 11:58:46 +0200

    Network

---
+++ CardGame/Assets/Gregoire/Scripts/NetWorking/NetWorkPlayer.cs
@@ -0,0 +1,50 @@
+using MLAPI;
+using MLAPI.Messaging;
+using MLAPI.NetworkVariable;
+using UnityEngine;
+
+public class NetWorkPlayer : NetworkBehaviour
+{
+    public NetworkVariableVector3 Position = new NetworkVariableVector3(new NetworkVariableSettings
+    {
+        WritePermission = NetworkVariablePermission.ServerOnly,
+        ReadPermission = NetworkVariablePermission.Everyone
+    });
+
+    public override void NetworkStart()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            var randomPosition = GetRandomPositionOnPlane();
+            transform.position = randomPosition;
+            Position.Value = randomPosition;
+            Debug.Log("Je passe dans move et je sui isServer");
+        }
+        else
+        {
+            SubmitPositionRequestServerRpc();
+            Debug.Log("si on est client");
+        }
+    }
+
+    [ServerRpc]
+    void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
+    {
+        Position.Value = GetRandomPositionOnPlane();
+    }
+
+    static Vector3 GetRandomPositionOnPlane()
+    {
+        return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+    }
+
+    void Update()
+    {
+        transform.position = Position.Value;
+    }
+}
