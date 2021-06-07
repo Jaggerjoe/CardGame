7bfe30095e823ae6975ece28b0617569b0b621eb
Carla <carlita.b34@gmail.com>
2021-06-07 14:41:24 +0200

    test

--- CardGame/Assets/Gregoire/Scripts/NetWorking/BoardNetWork/ActionInTheBoard.cs
+++ CardGame/Assets/Gregoire/Scripts/NetWorking/BoardNetWork/ActionInTheBoard.cs
@@ -27,8 +27,9 @@ namespace NetWork
                 s_LocalInstance = Instantiate(m_BoardReference);
                 if (IsHost)
                 {
-                    //s_LocalInstance.Side.m_Deck.Shuffle();
+                    //on recupere les deck et la postion de leur indexe
                     s_LocalInstance.SetDeckSideAndOtherSide();
+                    //puis on shuffle
                     s_LocalInstance.Side.m_Deck.Shuffle();
                     s_LocalInstance.Side2.m_Deck.Shuffle();
                     Debug.Log("isserver");
@@ -38,6 +39,7 @@ namespace NetWork
                     //appele de la fonction server rpc pour recup les info a envoyer au client
                     Debug.Log("client");
                     RequestServerRpc();
+                    DrawCard();
                 }
             }
         }
@@ -45,10 +47,10 @@ namespace NetWork
 
        
 
-        #region get index du deck 2 
+        #region get index du other deck
  
         //  Utilisé par les clients pour indiquer au serveur qu'un message doit être diffusé.
-        //parcousla totaliter du deck et pour chaque card il va prrendre le setindexCard et on  va recup 
+        //parcours la totaliter du deck et pour chaque card il va prrendre le setindexCard et on  va recup 
         [ServerRpc]
         private void RequestServerRpc()
         {
@@ -56,11 +58,17 @@ namespace NetWork
             //boucle pour parcourir le deck du side 2
             for (int i = 0; i < s_LocalInstance.Side2.m_Deck.Count; i++)
             {
+                //on ajoute le other deck au client
                 AddCardToDeckClientRpc(s_LocalInstance.Side2.m_Deck[i].m_Index);
             }
-
             Debug.Log("je suis dans la server Rpc" + s_LocalInstance.Side2.m_Deck[0].m_Index);
-            //DebugFirstDeckCardNumbeClientRpc();
+
+            for (int i = 0; i < s_LocalInstance.Side.m_Deck.Count; i++)
+            {
+                //on ajoute le other deck au client
+                AddCardToOtherDeckClientRpc(s_LocalInstance.Side.m_Deck[i].m_Index);
+            }
+            Debug.Log("boucle 2 je suis dans la server Rpc" + s_LocalInstance.Side.m_Deck[0].m_Index); 
         }
 
         [ClientRpc]
@@ -78,42 +86,146 @@ namespace NetWork
 
 
         //Utilisé par le serveur pour diffuser un message à tous les clients.
+        //on ajoute les au deck du serveur au client
         [ClientRpc]
         private void AddCardToDeckClientRpc(int p_IDCard)
         {
+            //pour chaque ellement de ma liste d'asset card
             for (int i = 0; i < s_LocalInstance.AllCards.Count; i++)
             {
+                //si p_idcard est egale a la liste a l'element i de l'index
                 if(p_IDCard == s_LocalInstance.AllCards[i].m_Index)
                 {
+                    //si le side du client est sup ou egale a 22
                     if(s_LocalInstance.Side.m_Deck.Count >= 22)
                     {
+                        //on ne fait rien
                         return;
                     }
                     else
                     {
+                        //si l'id du joueur est egale au local client id 
                         if(OwnerClientId == NetworkManager.Singleton.LocalClientId)
                         {
+                            //alors on ajoute au deck de ce joueur la liste dans le meme ordre avec i
                             s_LocalInstance.Side.m_Deck.Add(s_LocalInstance.AllCards[i]);
                             Debug.Log("j'ai le même ID ");
                         }
                         else
                         {
                             Debug.Log("je n'ai pas le même ID ");
+                            //sinon on ajoute au deck de l'autre joueur la liste aussi
                             s_LocalInstance.Side2.m_Deck.Add(s_LocalInstance.AllCards[i]);
                         }
                         //s_LocalInstance.SetDeck(s_LocalInstance.AllCards[i],OwnerClientId);
                     }
+                    //puis on sort du if
                     break;
                 }
-                else
+            }
+        }
+
+        [ClientRpc]
+        private void AddCardToOtherDeckClientRpc(int p_IDCard)
+        {
+            //pour chaque ellement de ma liste d'asset card
+            for (int i = 0; i < s_LocalInstance.AllCards.Count; i++)
+            {
+                //si p_idcard est egale a la liste a l'element i de l'index
+                if (p_IDCard == s_LocalInstance.AllCards[i].m_Index)
                 {
-                    Debug.LogError("je ne l'ai pas trouvé");
+                    //si le side du client est sup ou egale a 22
+                    if (s_LocalInstance.Side2.m_Deck.Count >= 22)
+                    {
+                        //on ne fait rien
+                        return;
+                    }
+                    else
+                    {
+                        //si l'id du joueur est egale au local client id 
+                        if (OwnerClientId == NetworkManager.Singleton.LocalClientId)
+                        {
+                            //alors on ajoute au deck de ce joueur la liste dans le meme ordre avec i
+                            s_LocalInstance.Side2.m_Deck.Add(s_LocalInstance.AllCards[i]);
+                            Debug.Log("j'ai le même ID ");
+                        }
+                        else
+                        {
+                            Debug.Log("je n'ai pas le même ID ");
+                            //sinon on ajoute au deck de l'autre joueur la liste aussi
+                            s_LocalInstance.Side.m_Deck.Add(s_LocalInstance.AllCards[i]);
+                        }
+                        //s_LocalInstance.SetDeck(s_LocalInstance.AllCards[i],OwnerClientId);
+                    }
+                    //puis on sort du if
+                    break;
                 }
             }
-            //on met le other side du host dans le side du client et inversement
-         
-            //Debug.Log($"l'ID de ma carte client RPC est {p_IDCard}");
+        }
+        #endregion
 
+        
+
+        #region Pioche Card
+        public void DrawCard()
+        {
+            if (IsServer)
+            {
+                DrawCardClientRpc(s_LocalInstance.Side2.m_Deck[0].m_Index);
+                Debug.Log("in drawn card is server ma indexe clientrpc est " + s_LocalInstance.Side2.m_Deck[0].m_Index);
+            }
+            else
+            {
+                DrawCardServerRpc();
+            }
+        }
+
+        [ServerRpc]
+        public void DrawCardServerRpc()
+        {
+            //je pioche une carte;
+            s_LocalInstance.DrawCardBoard(OwnerClientId);
+            Debug.Log("alut");
+            //je boucle pour piocher et mettre au bon emplacmeent la card
+            //for (int i = 0; i < s_LocalInstance.Side2.m_Deck.Count; i++)
+            for (int i = 0; i<s_LocalInstance.Side.m_Hand.Count; i++)
+            {
+                //j'appele la clientRpc
+                DrawCardClientRpc(s_LocalInstance.Side.m_Deck[i].m_Index);
+                Debug.Log("draw serverRpc in boucle");
+                break;
+            }
+            for(int i = 0; i<s_LocalInstance.Side2.m_Hand.Count; i++)
+            {
+                //j'appele la clientRpc
+                DrawCardClientRpc(s_LocalInstance.Side2.m_Deck[i].m_Index);
+                Debug.Log("draw serverRpc in boucle");
+                break;
+            }
+            Debug.Log("aurevoir");
+        }
+        [ClientRpc]
+        public void DrawCardClientRpc(int p_IdCard)
+        {
+            //je pioche une carte;
+            s_LocalInstance.DrawCardBoard(OwnerClientId);
+            Debug.Log("alut");
+
+            for (int i = 0; i < s_LocalInstance.Side.m_Hand.Count; i++)
+            {
+                //j'appele la clientRpc
+                p_IdCard= s_LocalInstance.Side.m_Deck[i].m_Index;
+                Debug.Log("draw clientRpc in boucle");
+                break;
+            }  
+            for (int i = 0; i < s_LocalInstance.Side2.m_Hand.Count; i++)
+            {
+                //j'appele la clientRpc
+                p_IdCard= s_LocalInstance.Side2.m_Deck[i].m_Index;
+                Debug.Log("draw clientRpc in boucle");
+                break;
+            }
+            Debug.Log("aurevoir");
         }
         #endregion
 
@@ -152,35 +264,6 @@ namespace NetWork
         }
 
         #endregion
-
-        #region Pioche Card
-        public void DrawCard(ulong p_PlayerNetworkID, int p_IDCard)
-        {
-            if (IsServer)
-            {
-                DrawCardClientRpc(p_PlayerNetworkID, p_IDCard);
-            }
-            else
-            {
-                DrawCardServerRpc(p_PlayerNetworkID, p_IDCard);
-            }
-        }
-
-        [ServerRpc]
-        public void DrawCardServerRpc(ulong p_PlayerNetworkID, int p_IDCard)
-        {
-            DrawCardClientRpc(p_PlayerNetworkID, p_IDCard);
-            Debug.Log("ta mere le cookie");
-        }
-        [ClientRpc]
-        public void DrawCardClientRpc(ulong p_PlayerNetworkID, int p_IDCard)
-        {
-
-            s_LocalInstance.DrawCardBoard(p_PlayerNetworkID, p_IDCard);
-            Debug.Log("je suis la coucou");
-        }
-        #endregion
-
         public SO_Board BoardInstance
         {
             get { return s_LocalInstance; }
--- CardGame/Assets/Gregoire/Scripts/NetWorking/BoardNetWork/Editor/ActionInTheBoardEditor.cs
+++ CardGame/Assets/Gregoire/Scripts/NetWorking/BoardNetWork/Editor/ActionInTheBoardEditor.cs
@@ -9,10 +9,14 @@ public class ActionInTheBoardEditor : Editor
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
+        //si on est en play
         if(EditorApplication.isPlaying)
         {
+            //on fait un espace
             EditorGUILayout.Space();
+            //on target action in the board
             ActionInTheBoard actionInTheBoard = target as ActionInTheBoard;
+            //si il est different egale de null
             if (actionInTheBoard.BoardInstance != null)
             {
                 SerializedObject obj = new SerializedObject(actionInTheBoard.BoardInstance);
